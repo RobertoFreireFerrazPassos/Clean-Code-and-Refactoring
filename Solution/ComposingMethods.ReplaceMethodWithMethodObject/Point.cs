d8dd61345538fa6edc1f54366a46cdc48f5e1406
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2022-01-10 19:28:04 -0300

    Replace Method with Method Object [Problem]

---
+++ Solution/ComposingMethods.ReplaceMethodWithMethodObject/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ComposingMethods.ReplaceMethodWithMethodObject
+{
+    /*
+     * 
+     * Problem:
+     * You have a long method in which the local variables are so intertwined that you can’t apply Extract Method.
+    */
+    public class Point
+    {
+        private double Latitude;
+        private double Longitude;
+
+        public Point(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public double CalculateDistance(Point anotherPoint)
+        {
+            double radian = 180 / 3.1415;
+            var radiansLatitude = this.Latitude / radian;
+            var anotherRadiansLatitude = anotherPoint.Latitude / radian;
+            var radiansLongitude = this.Longitude / radian;
+            var anotherRadiansLongitude = anotherPoint.Longitude / radian;
+            var radius = 6371;
+            var arc = Math.Acos(Math.Sin(radiansLatitude) * Math.Sin(anotherRadiansLatitude)
+                      + Math.Cos(radiansLatitude) * Math.Cos(anotherRadiansLatitude)
+                         * Math.Cos(radiansLongitude - anotherRadiansLongitude));
+            return Math.Round(arc * radius,3);
+        }
+    }
+}
+
---
+++ Solution/Test/Composing Methods/Replace Method with Method Object/PointTest.cs
@@ -0,0 +1,21 @@
+﻿using ComposingMethods.ReplaceMethodWithMethodObject;
+using Xunit;
+
+namespace Test.Composing_Methods.Replace_Method_with_Method_Object
+{
+    public class PointTest
+    {
+        [Fact(DisplayName = "Must Calculate Distance")]
+        [Trait("Replace Method with Method Object", "Point")]
+        public void MustCalculateDistance()
+        {
+            double expectedDistance = 16.113;
+            var pointA = new Point(-20.021937, -44.057844);
+            var pointB = new Point(-19.936868, -43.933015);
+
+            var result = pointA.CalculateDistance(pointB);
+
+            Assert.Equal(expectedDistance, result);
+        }
+    }
+}
